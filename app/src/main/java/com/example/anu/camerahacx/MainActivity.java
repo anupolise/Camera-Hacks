441f03a369c055aec4d0aa7add931fcaa3344ed7
anupolise <anu.polisetty444@gmail.com>
2019-01-11 00:32:17 -0800

    added camera on app open

--- app/src/main/java/com/example/anu/camerahacx/MainActivity.java
+++ app/src/main/java/com/example/anu/camerahacx/MainActivity.java
@@ -1,13 +1,68 @@
 package com.example.anu.camerahacx;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int MY_CAMERA_REQUEST_CODE = 100;
+    public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    //View v =  findViewById(R.layout.activity_main);
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+         setContentView(R.layout.activity_main);
+
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+        if(permissionCheck == PackageManager.PERMISSION_GRANTED){
+
+
+        } else if (permissionCheck != PackageManager.PERMISSION_GRANTED){
+            //Permission not granted, ask for permission
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},
+                    MY_CAMERA_REQUEST_CODE);
+        }
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        ImageView imgView = new ImageView(this);
+        Bitmap bitmapPhoto = null;
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+
+                bitmapPhoto = (Bitmap) data.getExtras().get("data");
+                Log.d("dog", "we took pic");
+
+            } else {
+                Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
 }
