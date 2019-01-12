c556594d7c1f26977d0ac28f38f22069b522e0bd
anupolise <anu.polisetty444@gmail.com>
2019-01-11 17:47:32 -0800

    added image labelling and firebasegit status

--- app/src/main/java/com/example/anu/camerahacx/MainActivity.java
+++ app/src/main/java/com/example/anu/camerahacx/MainActivity.java
@@ -7,6 +7,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -16,15 +17,32 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.label.FirebaseVisionLabel;
+import com.google.firebase.ml.vision.label.FirebaseVisionLabelDetector;
+import com.google.firebase.ml.vision.label.FirebaseVisionLabelDetectorOptions;
+
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int MY_CAMERA_REQUEST_CODE = 100;
     public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034;
     static final int REQUEST_IMAGE_CAPTURE = 1;
-    //View v =  findViewById(R.layout.activity_main);
+    Task<List<FirebaseVisionLabel>> result = null;
+
+    FirebaseVisionLabelDetectorOptions options =
+            new FirebaseVisionLabelDetectorOptions.Builder()
+                    .setConfidenceThreshold(0.8f)
+                    .build();
+
 
 
 
@@ -32,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
          setContentView(R.layout.activity_main);
+        FirebaseApp.initializeApp(getApplicationContext());
 
         int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
         if(permissionCheck == PackageManager.PERMISSION_GRANTED){
@@ -54,14 +73,54 @@ public class MainActivity extends AppCompatActivity {
         Bitmap bitmapPhoto = null;
         if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
             if (resultCode == RESULT_OK) {
-
                 bitmapPhoto = (Bitmap) data.getExtras().get("data");
-                Log.d("dog", "we took pic");
+                //not needed
+                imgView.setImageBitmap(bitmapPhoto);
+
+
+                FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmapPhoto);
+                FirebaseVisionLabelDetector detector = FirebaseVision.getInstance()
+                        .getVisionLabelDetector();
+
+                result =
+                        detector.detectInImage(image)
+                                .addOnSuccessListener(
+                                        new OnSuccessListener<List<FirebaseVisionLabel>>() {
+                                            @Override
+                                            public void onSuccess(List<FirebaseVisionLabel> labels) {
+                                                // Task completed successfully
+                                                // ...
+                                                printResults();
+                                            }
+                                        })
+                                .addOnFailureListener(
+                                        new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                // Task failed with an exception
+                                                // ...
+                                            }
+                                        });
+
+                Log.d("ANU", "we took pic");
+
+
 
             } else {
                 Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
             }
         }
+    }
+
+    public void printResults(){
+        for (FirebaseVisionLabel label:result.getResult())
+        {
+            String text = label.getLabel();
+            String entityId = label.getEntityId();
+            float confidence = label.getConfidence();
+            Log.d("VALUES",text +" "+confidence);
+        }
+
     }
 
 
