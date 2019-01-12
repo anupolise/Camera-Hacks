74c74f1ff51fe7528e9a4be4041cf447178eec1f
anupolise <anu.polisetty444@gmail.com>
2019-01-11 22:39:11 -0800

    added save and camera

--- app/src/main/java/com/example/anu/camerahacx/MainActivity.java
+++ app/src/main/java/com/example/anu/camerahacx/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.anu.camerahacx;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
@@ -27,8 +29,12 @@ import com.google.firebase.ml.vision.label.FirebaseVisionLabel;
 import com.google.firebase.ml.vision.label.FirebaseVisionLabelDetector;
 import com.google.firebase.ml.vision.label.FirebaseVisionLabelDetectorOptions;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -37,6 +43,8 @@ public class MainActivity extends AppCompatActivity {
     public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034;
     static final int REQUEST_IMAGE_CAPTURE = 1;
     Task<List<FirebaseVisionLabel>> result = null;
+    public final String APP_TAG = "CameraHacx";
+
 
     FirebaseVisionLabelDetectorOptions options =
             new FirebaseVisionLabelDetectorOptions.Builder()
@@ -74,9 +82,14 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
             if (resultCode == RESULT_OK) {
                 bitmapPhoto = (Bitmap) data.getExtras().get("data");
-                //not needed
                 imgView.setImageBitmap(bitmapPhoto);
+                imgView.setVisibility(View.VISIBLE);
 
+                // store img
+                MediaStore.Images.Media.insertImage(getContentResolver(), bitmapPhoto, "IMG"+new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date()) , "");
+
+
+                createDirectoryAndSaveFile(generateFileName(), imgView);
 
                 FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmapPhoto);
                 FirebaseVisionLabelDetector detector = FirebaseVision.getInstance()
@@ -91,6 +104,7 @@ public class MainActivity extends AppCompatActivity {
                                                 // Task completed successfully
                                                 // ...
                                                 printResults();
+
                                             }
                                         })
                                 .addOnFailureListener(
@@ -102,7 +116,15 @@ public class MainActivity extends AppCompatActivity {
                                             }
                                         });
 
+                Toast.makeText(this, "Picture saved!", Toast.LENGTH_LONG).show();
+
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+
                 Log.d("ANU", "we took pic");
+
+
+
 
 
 
@@ -121,6 +143,39 @@ public class MainActivity extends AppCompatActivity {
             Log.d("VALUES",text +" "+confidence);
         }
 
+    }
+
+    private String generateFileName()
+    {
+
+        return "Image"+"_"+new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date())+".jpg";
+
+    }
+
+    private void createDirectoryAndSaveFile(String fileName, ImageView ivView)
+    {
+        Bitmap img = ivView.getDrawingCache();
+        File mediaStorageDir = new File( getExternalFilesDir(Environment.DIRECTORY_PICTURES), APP_TAG);
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.d(APP_TAG, "failed to create directory");
+        }
+
+        File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
+
+        if (file.exists())
+        {
+            file.delete();
+        }
+
+        try {
+            FileOutputStream out = new FileOutputStream(file);
+            img.compress(Bitmap.CompressFormat.JPEG, 100, out);
+            out.flush();
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 
