00fce33b3b5028dbcabf7d0c3ab9842ab2686739
anupolise <anu.polisetty444@gmail.com>
2019-01-12 06:12:19 -0800

    some focusing was attempted..

--- app/src/main/java/com/example/anu/camerahacx/Camera_capture.java
+++ app/src/main/java/com/example/anu/camerahacx/Camera_capture.java
@@ -1,14 +1,22 @@
 package com.example.anu.camerahacx;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.hardware.Camera;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.util.Log;
 import android.view.Display;
+import android.view.MotionEvent;
 import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -17,12 +25,28 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.label.FirebaseVisionLabel;
+import com.google.firebase.ml.vision.label.FirebaseVisionLabelDetector;
+import com.google.firebase.ml.vision.label.FirebaseVisionLabelDetectorOptions;
+
 public class Camera_capture extends Activity implements SurfaceHolder.Callback {
 
     private Camera mCamera;
     private SurfaceView surfaceView;
     private SurfaceHolder surfaceHolder;
     private Button capture_image;
+
+    Task<List<FirebaseVisionLabel>> result = null;
+    FirebaseVisionLabelDetectorOptions options =
+            new FirebaseVisionLabelDetectorOptions.Builder()
+                    .setConfidenceThreshold(0.8f)
+                    .build();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +78,8 @@ public class Camera_capture extends Activity implements SurfaceHolder.Callback {
         }
     }
 
+
+
     private void capture() {
         mCamera.takePicture(null, null, null, new Camera.PictureCallback() {
 
@@ -64,14 +90,48 @@ public class Camera_capture extends Activity implements SurfaceHolder.Callback {
                 Intent intent = new Intent();
                 intent.putExtra("image_arr", data);
                 setResult(RESULT_OK, intent);
-                camera.stopPreview();
-                if (camera != null) {
-                    //camera.release();
-                    mCamera = null;
-                }
-                finish();
+                Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+                FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
+                FirebaseVisionLabelDetector detector = FirebaseVision.getInstance()
+                        .getVisionLabelDetector();
+
+                result =
+                        detector.detectInImage(image)
+                                .addOnSuccessListener(
+                                        new OnSuccessListener<List<FirebaseVisionLabel>>() {
+                                            @Override
+                                            public void onSuccess(List<FirebaseVisionLabel> labels) {
+                                                // Task completed successfully
+                                                // ...
+                                                printResults();
+
+                                            }
+                                        })
+                                .addOnFailureListener(
+                                        new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                // Task failed with an exception
+                                                // ...
+                                            }
+                                        });
+                camera.startPreview();
+
             }
         });
+
+    }
+
+
+    public void printResults(){
+        for (FirebaseVisionLabel label:result.getResult())
+        {
+            String text = label.getLabel();
+            String entityId = label.getEntityId();
+            float confidence = label.getConfidence();
+            Log.d("VALUES",text +" "+confidence);
+        }
+
     }
 
     @Override
@@ -80,6 +140,11 @@ public class Camera_capture extends Activity implements SurfaceHolder.Callback {
         Log.e("Surface Changed", "format   ==   " + format + ",   width  ===  "
                 + width + ", height   ===    " + height);
         mCamera.setDisplayOrientation(90);
+        Camera.Parameters p = mCamera.getParameters();
+        p.setFocusMode(Camera.Parameters.FOCUS_MODE_AUTO);
+        p.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
+
+
         try {
             mCamera.setPreviewDisplay(holder);
             mCamera.startPreview();
@@ -96,6 +161,10 @@ public class Camera_capture extends Activity implements SurfaceHolder.Callback {
         Log.e("Surface Created", "");
         mCamera=Camera.open();
         mCamera.setDisplayOrientation(90);
+        Camera.Parameters p = mCamera.getParameters();
+        p.setFocusMode(Camera.Parameters.FOCUS_MODE_AUTO);
+        p.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
+                                         
 
     }
 
